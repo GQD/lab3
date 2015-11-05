475354d5fb27d8928d1b5bce77f43566dc93a59f
gara <g@hotmail.com>
2015-11-05 19:26:00 +0000

    version1.1

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('pagina en construccion');
+  res.send('respond with a resource');
 });
 
 module.exports = router;
